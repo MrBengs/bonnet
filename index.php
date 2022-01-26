aee209df340ee43297590bb491ff7f6fe4fcf605
jbenga <jpaul.benga@gmail.com>
2022-01-26 16:11:13 +0100

    exercice 3

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 $bonnets = [
     "Bonnet en laine" =>
-    ["prix" => "10€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
+    ["prix" => 10, "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
 
     "Bonnet en laine bio" =>
-    ["prix" => "14€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
+    ["prix" => 14, "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
 
     "Bonnet en laine et cachemire" =>
-    ["prix" => "20€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
+    ["prix" => 20, "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
 
     "Bonnet arc-en-ciel" =>
-    ["prix" => "12€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."]
+    ["prix" => 12, "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."]
 ]
 
 ?>
@@ -25,6 +25,7 @@ $bonnets = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bonnet Php</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
@@ -34,7 +35,13 @@ $bonnets = [
         ?>
             <tr>
                 <td><?= "{$key}" ?></td>
-                <td>Prix:<?= "{$value['prix']}" ?></td>
+                <td <?php if ($value['prix'] < 12) {
+                        echo 'class=text-success';
+                    } else {
+                        echo 'class=text-primary';
+                    }
+                    ?>>Prix:<?= "{$value['prix']}" ?></td>
+
                 <td>description:<?= "{$value['description']}" ?></td>
             </tr>
         <?php
