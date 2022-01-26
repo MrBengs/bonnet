5159f523db960ef689e5ca133211b018242fd643
jbenga <jpaul.benga@gmail.com>
2022-01-26 15:50:21 +0100

    exercice 2

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
 
-$bonnets = array(
-    1 => "Bonnet en laine",
-    2 =>  "Bonnet en laine bio",
-    3 => "Bonnet en laine et cachemire",
-    4 => "Bonnet arc-en-ciel"
-);
+$bonnets = [
+    "Bonnet en laine" =>
+    ["prix" => "10€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
+
+    "Bonnet en laine bio" =>
+    ["prix" => "14€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
+
+    "Bonnet en laine et cachemire" =>
+    ["prix" => "20€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."],
+
+    "Bonnet arc-en-ciel" =>
+    ["prix" => "12€", "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a leo diam. Quisque lorem orci, accumsan quis dolor sed, gravida."]
+]
 
 ?>
 
@@ -13,22 +20,27 @@ $bonnets = array(
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bonnet Php</title>
 </head>
+
 <body>
     <table>
-    <?php
-    foreach($bonnets as $key => $value){
-    ?> 
-    <tr>
-    <td><?= "{$key} => {$value}"?></td>
-    </tr>
-    <?php
-    }
-    ?>
+        <?php
+        foreach ($bonnets as $key => $value) {
+        ?>
+            <tr>
+                <td><?= "{$key}" ?></td>
+                <td>Prix:<?= "{$value['prix']}" ?></td>
+                <td>description:<?= "{$value['description']}" ?></td>
+            </tr>
+        <?php
+        }
+        ?>
     </table>
 </body>
+
 </html>
