890883cb0427c29e05342e982092134a6ae335b5
jbenga <jpaul.benga@gmail.com>
2022-01-27 11:24:17 +0100

    exercice 11

---
+++ list.php
@@ -0,0 +1,24 @@
+<?php require_once('includes/functions.php'); ?>
+<?php require_once('includes/variables.php'); ?>
+<?php require_once('includes/header.php'); ?>
+
+
+
+<div class="container-fluid col-6">
+    <table class="table table-bordered border-secondary table-striped table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Nom</th>
+                <th scope="col">Prix HT</th>
+                <th scope="col">Prix TTC</th>
+                <th scope="col">Description</th>
+            </tr>
+        </thead>
+        <?php
+        foreach ($bonnets as $key => $value) {
+            displayProduct($key, $value);
+        }
+        ?>
+    </table>
+</div>
+<?php require_once('includes/footer.php'); ?>
