fd1814a016411751713dd509ec64ec6055f4b6b1
jbenga <jpaul.benga@gmail.com>
2022-01-27 12:03:44 +0100

    creation de la branche

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php require_once('includes/header.php'); ?>
 
 
-<div class="container-fluid bg-dark d-flex">
+<div class="container-fluid bg-light d-flex">
     <?php
     foreach (array_slice($bonnets, 0, 3, true) as $name => $value) {
         displayCards($name, $value);
     }
     ?>
+
 </div>
+<div class="container-fluid bg-light">
+    <a class="btn btn-outline-primary mt-5 ms-2" href="list.php" role="button">List</a>
+</div>
+
 
 <?php require_once('includes/footer.php'); ?>
