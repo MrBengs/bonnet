545d6167b655d20830973b1daa124a31b1d102f5
jbenga <jpaul.benga@gmail.com>
2022-01-27 14:11:11 +0100

    exercice 15

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,24 @@
     <title>Bonnet Php</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+
 </head>
 
 <body>
+    <ul class="nav justify-content-end mb-3">
+        <?php
+        if (isset($_POST['id'])) {
+        ?>
+            <li class="nav-item">Bienvenue <?= $_POST['id']; ?></li>
+        <?php
+        }
+        ?>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Acceuil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="list.php">List</a>
+        </li>
+    </ul>
